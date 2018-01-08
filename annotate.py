1501ffb3d82aeeb5afad0aa1f96837c7f3fbc6b5
Owen Temple <owentemple@gmail.com>
2018-01-08 14:44:25 -0600

    write functions to fill null occupations, remove multiple speakers, and drop records with no transcript

--- annotate.py
+++ annotate.py
@@ -2,11 +2,16 @@ import os
 import settings
 import pandas as pd
 
-def add_long_transcripts():
-    '''
-    Adds transcripts for 3 talks whose transcripts exceeded csv single cell capacity
-    '''
+def read_data():
     df = pd.read_excel(os.path.join(settings.PROCESSED_DIR, "all.xls"), encoding="ISO-8859-1")
+    df = df.reset_index()
+    return df
+
+
+def add_long_transcripts(df):
+    '''
+    Adds transcripts for 4 talks whose transcripts exceed max csv single cell capacity of 32767 characters
+    '''
     long_transcripts = {354: 'dan_gilbert_researches_happiness.txt', \
                         2441: 'elon_musk_the_future_we_re_building_and_boring.txt', \
                         2421: 'gretchen_carlson_david_brooks_political_common_ground_in_a_polarized_united_states.txt', \
@@ -17,7 +22,21 @@ def add_long_transcripts():
             data = data.replace('\t', '')
             data = data.replace('\n', '')
             df.at[k, 'transcript'] = data
+    return df
+
+def fill_null_occupations():
+    df['speaker_occupation'].fillna('No Occupation Noted', axis=0, inplace=True)
+
+def remove_single_speaker():
+    return df[df['num_speaker'] == 1]
+
+def drop_no_transcript():
+    return df.dropna(axis=0)
 
 
 if __name__ == "__main__":
-    add_long_transcripts()
+    df = read_data()
+    df = add_long_transcripts(df)
+    fill_null_occupations()
+    df = remove_single_speaker()
+    df = drop_no_transcript()
