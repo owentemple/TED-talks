2f1ae33896b609d7e62325f4d716bcc4d48f4dbf
Owen Temple <owentemple@gmail.com>
2018-01-08 15:34:26 -0600

    write functions to create audience rating columns and remove parenthetical expressions from transcript

--- annotate.py
+++ annotate.py
@@ -27,16 +27,56 @@ def add_long_transcripts(df):
 def fill_null_occupations():
     df['speaker_occupation'].fillna('No Occupation Noted', axis=0, inplace=True)
 
-def remove_single_speaker():
+def remove_multiple_speakers():
     return df[df['num_speaker'] == 1]
 
 def drop_no_transcript():
     return df.dropna(axis=0)
+
+def trim_q_and_a(x, pattern, offset):
+    trim_index = x.find(pattern, 400)
+    if trim_index == -1:
+        return x
+    return x[:trim_index+offset]
+
+def parse_ratings(x, label):
+    x = ast.literal_eval(x)
+    for row in x:
+        if row['name'] == label:
+            return int(row['count'])
+    else:
+        return 0
+
+
+def count_audience_reaction(x, term):
+    return x.count(term)
+
+def create_new_columns():
+    # Create columns from ratings
+    df['persuasive'] = df['ratings'].apply(parse_ratings, args=('Persuasive',))
+    df['inspiring'] = df['ratings'].apply(parse_ratings, args=('Inspiring',))
+    df['unconvincing'] = df['ratings'].apply(parse_ratings, args=('Unconvincing',))
+    df['applause'] = df['transcript'].apply(count_audience_reaction, args=('(Applause)',))
+    df['laughter'] = df['transcript'].apply(count_audience_reaction, args=('(Laughter)',))
+    return df
+
+def remove_parenthetical(x):
+    return re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", x)
+
 
 
 if __name__ == "__main__":
     df = read_data()
     df = add_long_transcripts(df)
     fill_null_occupations()
-    df = remove_single_speaker()
+    df = remove_multiple_speakers()
     df = drop_no_transcript()
+    # Removes the Q and A session from end of transcripts
+    df['transcript'] = df['transcript'].apply(trim_q_and_a, args=('(Applause)Chris Anderson:', 10))
+    df['transcript'] = df['trim_transcript'].apply(trim_q_and_a, args=('(Applause) Chris Anderson:', 10))
+    ## Reimport dataframe after cleaning transcript and adding 2 new columns manually
+    df = pd.read_excel(os.path.join(settings.PROCESSED_DIR, "all_with_transcript_edited.xls"), encoding="ISO-8859-1")
+    df = create_new_columns()
+    df['transcript'] = df['transcript'].apply(remove_parenthetical)
+
+
