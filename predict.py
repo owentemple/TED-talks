08c04835f2ab3b43a53f42a504631d2488be9a13
Owen Temple <owentemple@gmail.com>
2018-01-10 17:44:09 -0600

    write linear regression script to calculate mean squared error

--- predict.py
+++ predict.py
@@ -2,15 +2,33 @@ import os
 import settings
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
 def read_data():
-    df = pd.read_excel(os.path.join(settings.PROCESSED_DIR, "all_with_liwc_segmented.xls"), encoding="ISO-8859-1")
-    return df
+    train = pd.read_excel(os.path.join(settings.PROCESSED_DIR, "all_with_liwc_segmented.xls"), encoding="ISO-8859-1")
+    return train
 
+def cross_validate(train):
+    clf = LinearRegression()
 
+    predictors = train.columns.tolist()
+    predictors = [p for p in predictors if p not in settings.NON_PREDICTORS]
 
+    predictions = cross_validation.cross_val_predict(clf, train[predictors], train[settings.TARGET], cv=settings.CV_FOLDS)
+    return predictions
+
+def compute_error(target, predictions):
+    return mean_squared_error(target, predictions)
 
 
 
 
 if __name__ == "__main__":
-    df = read_data()
+    train = read_data()
+    predictions = cross_validate(train)
+    error = compute_error(train[settings.TARGET], predictions)
+    print("Mean Squared Error: {}".format(error))
