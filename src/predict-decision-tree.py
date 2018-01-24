87cad1e278526ebae65a13bf5c997b07fc44e06c
Owen Temple <owentemple@gmail.com>
2018-01-23 21:38:39 -0600

    simplify build decision tree script

--- src/predict-decision-tree.py
+++ src/predict-decision-tree.py
@@ -7,38 +7,30 @@ import settings
 import pandas as pd
 import operator
 
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error
+
 
 def read_data():
-    df = pd.read_excel(os.path.join('..', settings.PROCESSED_DIR, "all_with_liwc_segmented.xls"), encoding="ISO-8859-1")
+    df = pd.read_excel(os.path.join('..',settings.PROCESSED_DIR, "all_with_liwc_segmented.xls"), encoding="ISO-8859-1")
     return df
-
-def create_test_set(df):
-    predictors = df.columns.tolist()
-    predictors = [p for p in predictors if p not in settings.NON_PREDICTORS]
-    X_train, X_test, y_train, y_test = train_test_split(df[predictors], df[settings.TARGET], random_state = 42)
-    return X_train, X_test, y_train, y_test
-
-
-def compute_error(target, predictions):
-    return mean_squared_error(target, predictions)
 
 def sort_important_features(df):
     dt = DecisionTreeRegressor()
     predictors = df.columns.tolist()
     predictors = [p for p in predictors if p not in settings.NON_PREDICTORS]
-    dt.fit(df[predictors], df[settings.TARGET])
-    predictions = dt.predict(df[predictors])
+    model = dt.fit(df[predictors], df[settings.TARGET])
     results = {name: score for name, score in zip(predictors, dt.feature_importances_)}
     sorted_results = sorted(results.items(), key=operator.itemgetter(1), reverse=True)
-    print(sorted_results)
+    print("Feature Importances for the Response Variable: {}".format(settings.TARGET))
+    for feat, importance in sorted_results:
+        print("feature: {f}, importance: {i}".format(f=feat, i=importance))
     accuracy = dt.score(df[predictors], df[settings.TARGET])
     print("Accuracy: {}".format(accuracy))
+    return model
 
 
 if __name__ == "__main__":
     df = read_data()
-    X_train, X_test, y_train, y_test = create_test_set(df)
-    sort_important_features(df)
+    model = sort_important_features(df)
+
+
